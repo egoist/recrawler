1761c18451000c8a23d37d13f3b26c51e2ef50cb
EGOIST <0x142857@gmail.com>
2016-05-05 14:07:08 +0800

    fix xo

--- test.js
+++ test.js
@@ -2,16 +2,16 @@ import test from 'ava'
 import aimer from './'
 
 test('ok', async t => {
-	const $ = await aimer('https://www.npmjs.com/')
-	const src = $('#npm-logo').find('img').attr('src')
-	t.is(src, '/static/images/npm-logo.svg')
+  const $ = await aimer('https://www.npmjs.com/')
+  const src = $('#npm-logo').find('img').attr('src')
+  t.is(src, '/static/images/npm-logo.svg')
 })
 
 test('404', async t => {
-	try {
-		await aimer('https://www.npmjs.com/sasdffsad')
-		t.fail('should throw 404 but didn\'t')
-	} catch (e) {
-		t.pass(e.message)
-	}
+  try {
+    await aimer('https://www.npmjs.com/sasdffsad')
+    t.fail('should throw 404 but didn\'t')
+  } catch (e) {
+    t.pass(e.message)
+  }
 })
